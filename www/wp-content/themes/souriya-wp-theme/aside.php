6c7625432335c2a114f816d55853b6fdd8dd18eb
Souriya PHONGSAVANH <benjamin.franklin59777@gmail.com>
2023-05-17 14:02:32 +0200

    profile picture is contain inside the theme folder (not from google drive anymore)

--- www/wp-content/themes/souriya-wp-theme/aside.php
+++ www/wp-content/themes/souriya-wp-theme/aside.php
@@ -1,5 +1,5 @@
 <aside>
-  <img src="https://lh3.googleusercontent.com/drive-viewer/AAOQEOTZceJd-89lhxJV7QhLpt9awsdmhAVOQp0kOtgGvINOd9z26CFQUs0ZWHSEmgE84JBQU-D9gduP0_825kaVMPiSXQcB=w1167-h944" alt="souriya profil" />
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/photo-profil-cropped.jpg" alt="image souriya profil" />
   <div class="text">
     <p>Blog personnel de <a class="cool" href="/a-propos/">Souriya</a>.</p>
     <p>Pensées pour moi-même.</p>
